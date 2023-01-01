237676b511d97dfa37b2c7f1a5b43426639da160
SergioAJS <SergFanMadrid@gmail.com>
2023-01-02 00:12:19 +0500

    feat: add copy URL button

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -3,18 +3,34 @@ import { useProducts } from '../../hooks/products'
 import { SelectCategory } from '../Select/SelectCategory'
 import { SelectBrand } from '../Select/SelectBrand'
 import MultiRangeSlider from '../MultiRangeSlider/MultiRangeSlider'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import AppContext from '../../context'
 
 export function Filters() {
   const { minPrice, maxPrice } = useProducts()
   const { onClearFilters } = useContext(AppContext)
+  const [copied, setCopied] = useState(false)
+
+  function onCopy() {
+    navigator.clipboard.writeText(window.location.href)
+    setCopied(true)
+    const timer = setTimeout(() => {
+      setCopied(false)
+    }, 700)
+    return () => clearTimeout(timer)
+  }
+
   return (
     <section className={styles.section}>
       <h2 className={styles.section_header}>Filters</h2>
-      <button className={styles.clear__filters} onClick={onClearFilters}>
-        Clear Filters
-      </button>
+      <div className={styles.buttons}>
+        <button className={styles.clear__filters} onClick={onClearFilters}>
+          Clear Filters
+        </button>
+        <button className={styles.copy} onClick={onCopy}>
+          {copied ? 'URL Copied' : 'Copy URL'}
+        </button>
+      </div>
       <SelectCategory />
       <SelectBrand />
       <MultiRangeSlider
