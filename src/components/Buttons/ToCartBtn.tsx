c0365b14f2983c5bbe7adb234589bfee6efd1d85
SergioAJS <SergFanMadrid@gmail.com>
2023-01-04 10:26:52 +0500

    fix: ToCart button styles

--- src/components/Buttons/ToCartBtn.tsx
+++ src/components/Buttons/ToCartBtn.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../Product/Product.module.scss'
+import styles from './ToCartBtn.module.scss'
 import AppContext from '../../context'
 import { ProductProps } from '../Product/Product'
 import { IProductInCart } from '../../models'
