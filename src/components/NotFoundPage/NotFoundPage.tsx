9dd29970011a6bdf37f125bee5546a31675b635c
Andrei <platonovaudit@yandex.ru>
2023-01-05 13:42:33 +0500

    feat: add 404 to page 404

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -3,7 +3,10 @@ import styles from './NotFoundPage.module.scss'
 export function NotFoundPage() {
   return (
     <main className="main">
-      <div className={styles.not__found}>Page not Found</div>
+      <div className={styles.not__found}>
+        Page not Found
+        <h1 className={styles.not__found_title}>404</h1>
+      </div>
     </main>
   )
 }
